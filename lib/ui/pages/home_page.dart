63d1bf7953a6f7c5a933a6c46c80b0fb62cca6d8
Putri Ayu Nisa Az Zahra <puayyssii12@gmail.com>
2024-12-19 18:18:03 +0800

    update: fix map position and add marker/annotation

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,11 +12,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  MapboxMap? mapbox;
+  PointAnnotationManager? pointAnnotationManager;
+  PointAnnotationOptions? pointAnnotationOptions;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: MapWidget(
-          styleUri: MapboxStyles.MAPBOX_STREETS,
           key: ValueKey("mapWidget"),
           onMapCreated: _onMapCreated,
           onCameraChangeListener: _onCameraChangeListener,
@@ -21,14 +27,34 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  _onMapCreated(MapboxMap mapboxmap) {
-    mapboxmap.setCamera(CameraOptions(
-        center: Point(coordinates: Position(-1.2385228248171842, 116.84736863711628)),
+  _onMapCreated(MapboxMap mapboxmap) async {
+    mapbox = mapboxmap;
+
+    mapbox?.setCamera(CameraOptions(
+        center: Point(coordinates: Position(116.84736863711628, -1.2385228248171842)),
         padding: MbxEdgeInsets( top: 40.0, left: 5.0, bottom: 80.0, right: 5.0),
-        zoom: 15.5,
-        bearing: -17.6,
+        zoom: 16.0,
+        bearing: -10,
         pitch: 60
-    ));
+    )
+    );
+    mapbox?.loadStyleURI(MapboxStyles.MAPBOX_STREETS);
+
+    pointAnnotationManager = await mapbox?.annotations.createPointAnnotationManager();
+
+    // Load the image from assets
+    final ByteData bytes = await rootBundle.load('assets/custom-icon.png');
+    final Uint8List imageData = bytes.buffer.asUint8List();
+
+    // Create a PointAnnotationOptions
+    pointAnnotationOptions = PointAnnotationOptions(
+        geometry: Point(coordinates: Position(116.84736863711628, -1.2385228248171842)), // Example coordinates
+        image: imageData,
+        iconSize: 1.5
+    );
+
+    // Add the annotation to the map
+    pointAnnotationManager?.create(pointAnnotationOptions!);
   }
 
   _onCameraChangeListener(CameraChangedEventData data) {
